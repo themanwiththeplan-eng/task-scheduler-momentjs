7a3ef051a267678af943bd25fc7df6900ed71f77
Dakota Dunn <dakotadunn5@gmail.com>
2022-01-23 17:39:24 -0800

    starting store function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,9 @@
 var today = new Date().toLocaleDateString();
 var momentToday = moment(today).format("dddd, MMMM Do YYYY");
 $("#currentDay").text(momentToday);
+
+$('.btn').on('click', store);
+
+function store(){
+    
+}
